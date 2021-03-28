6f246868361db79e9112c02164a03ef7b6538b86
Timofey Peshin <timoffex@gmail.com>
2021-03-28 00:28:47 -0700

    Disable physics debug display

--- main.py
+++ main.py
@@ -174,8 +174,6 @@ class Application:
 
         self.make_borders()
 
-        self._physics_system.debug_save_bounding_boxes = True
-
         last_ticks = pygame.time.get_ticks()
         while True:
             pygame.time.delay(20)
@@ -198,17 +196,17 @@ class Application:
             screen.fill(pygame.Color(0, 0, 0))
 
             # Draw quadtree constructed in physics step
-            for box in self._physics_system.debug_get_bounding_boxes():
-                pygame.gfxdraw.rectangle(
-                    screen,
-                    pygame.Rect(
-                        box.x_min,
-                        box.y_min,
-                        box.width,
-                        box.height,
-                    ),
-                    pygame.Color(255, 0, 255),
-                )
+            # for box in self._physics_system.debug_get_bounding_boxes():
+            #     pygame.gfxdraw.rectangle(
+            #         screen,
+            #         pygame.Rect(
+            #             box.x_min,
+            #             box.y_min,
+            #             box.width,
+            #             box.height,
+            #         ),
+            #         pygame.Color(255, 0, 255),
+            #     )
 
             self._rendering_system.render(screen)
             pygame.display.update()
